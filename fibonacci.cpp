640aafcbe11d05aeb7f061ef37614a3aebbdec35
Hanssell89 <hanssell.llanes16143@est.uca.edu.ni>
2023-06-05 08:09:23 -0600

    Se agrego el archivo de # fibonacci

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,3 +1,35 @@
 #include <iostream>
+
 using namespace std;
 
+void fibonacciSequence(int n) {
+    int first = 0, second = 1;
+    
+
+    if (n < 0) {
+        cout << "No se puede generar la secuencia de Fibonacci para un numero negativo." << endl;
+        return;
+    }
+    
+    cout << first << " " << second << " ";
+    
+    for (int i = 2; i <= n; i++) {
+        int next = first + second;
+        cout << next << " ";
+        
+        first = second;
+        second = next;
+    }
+    
+    cout << endl;
+}
+
+int main() {
+    int number;
+    cout << "Ingrese un numero entero: ";
+    cin >> number;
+    
+    fibonacciSequence(number);
+    
+    return 0;
+}
